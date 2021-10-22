31080f01c5fae4fd589bb62d2d111f70a72566eb
Vlad <chepurnojv@gmail.com>
2021-10-22 16:37:45 +0300

    Saving User Data

--- src/models/User.ts
+++ src/models/User.ts
@@ -41,9 +41,19 @@ export class User {
 
   fetch(): void {
     axios
-      .get(`https://localhost:3000/users/${this.get("id")}`)
+      .get(`http://localhost:3000/users/${this.get("id")}`)
       .then((response: AxiosResponse): void => {
         this.set(response.data);
       });
+  }
+
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post(`http://localhost:3000/users`, this.data);
+    }
   }
 }
