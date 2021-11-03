7b92a741e5d94d475df9c34ee7d610b23c081049
Vlad <chepurnojv@gmail.com>
2021-11-03 18:08:43 +0200

    Passthrough Methods

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { User } from "./models/User";
 
 const user = new User({ name: "new record", age: 0 });
 
-user.sync.save();
+user.on;
+
+// user.sync.save();
 
 // user.events.on("change", () => {
 //   console.log("change?");
