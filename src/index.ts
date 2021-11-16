c781cb54e1067ba7895faf92f9744a760b2e7d03
Vlad <chepurnojv@gmail.com>
2021-11-16 18:02:25 +0200

    Generic User Collection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import { User } from "./models/User";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+const collection = new Collection<User, UserProps>(
+  "http://localhost:3000/users",
+  (json: UserProps) => User.buildUser(json)
+);
 
-const user = User.buildUser({ id: 1 });
-
-user.on("change", () => {
-  console.log("user");
-});
-
-user.fetch();
+collection.fetch();
+// import { User } from "./models/User";
+// const user = User.buildUser({ id: 1 });
+// user.on("change", () => {
+//   console.log("user");
+// });
+// user.fetch();
