779cd09ba5dbdc900cf05d58ecae5faaaba16053
unknown <chepurnojv@gmail.com>
2021-11-27 17:27:19 +0200

    Strict Null Checks

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,37 +1,35 @@
-import {User} from '../models/User'
+import { User } from "../models/User";
 
 export class UserForm {
-  constructor(public parent: Element, public model:User) {
-    this.bindModel()
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
   }
 
-  bindModel():void{
-    this.model.on('change',()=>{
-      this.render()
-    })
-
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
   }
 
-eventsMap(): {[key:string]: ()=>void} {
-  return {
-    'click:.set-age':this.onSetAgeClick,
-    'click:.set-name':this.onSetNameClick,
-
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:.set-age": this.onSetAgeClick,
+      "click:.set-name": this.onSetNameClick,
+    };
   }
-}
 
-onSetNameClick=():void => {
-  const input = this.parent.querySelector('input')
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("input");
 
-  const name =input.value
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
+  };
 
-  this.model.set({name})
-}
-
-onSetAgeClick=():void => {
-  this.model.setRandomAge();
-}
-
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
@@ -49,25 +47,25 @@ onSetAgeClick=():void => {
         `;
   }
 
-  bindEvents(fragment:DocumentFragment):void {
-    const eventsMap=this.eventsMap()
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
 
-    for(let eventKey in eventsMap){
-      const [eventName, selector]=eventKey.split(':');
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
 
-      fragment.querySelectorAll(selector).forEach(element=>{
-        element.addEventListener(eventName, eventsMap[eventKey])
-      })
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
     }
   }
 
   render(): void {
-    this.parent.innerHTML=''
+    this.parent.innerHTML = "";
 
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
-    this.bindEvents(templateElement.content)
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
